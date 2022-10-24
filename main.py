4f71064345273a97a654386599bf8617720c6455
Potatopy <baricaz001@outlook.com>
2022-10-23 23:29:29 -0400

    added main ticket

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
-from nextcord.ext import commands
 import nextcord
+from nextcord.ext import commands
+import os
 import asyncio
-from config.json import TOKEN
+from dotenv import load_dotenv
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
 
-bot = commands.Bot(command_prefix='-', intents=nextcord.Intents.all())
+class CreateTicket(nextcord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
 
-@bot.event
-async def on_ready():
-    print('Bot is online!')
+    @nextcord.ui.button(
+        label = "Create Ticket", style=nextcord.ButtonStyle.blurple, custom_id="create_ticket:blurple"
+    )
+    async def create_ticket(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
+        await interaction.response.send_message("A ticket is being made for you :wink:", ephemeral=True)
+
+class Bot(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.persistent_views_added = False
+
+    async def on_ready(self):
+        if not self.persistent_views_added:
+            self.add_view(CreateTicket())
+            self.persistent_views_added = True
+            print("Persistent views added")
+            print(f"Logged in as {self.user}")
+
+
+bot = Bot(command_prefix='-', intents=nextcord.Intents.all())
+
+@bot.command()
+@commands.has_permissions(manage_guild=True)
+async def setup_tickets(ctx: commands.Context):
+    embed = nextcord.Embed(title="Need Support?", description="For Support Click the `Create Ticket` Button for support! And we'll respond within less than 24 hours.")
+    await ctx.send(embed=embed, view=CreateTicket())
 
 bot.run(TOKEN)
