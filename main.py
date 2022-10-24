fbaceaacc11a59b8be2667d467acbda21975255d
Potatopy <baricaz001@outlook.com>
2022-10-23 21:06:02 -0400

    commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from nextcord.ext import commands
+import nextcord
+import asyncio
+from config import TOKEN
+
+bot = commands.Bot(command_prefix='-', intents = nextcord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print('bot is ready!')
+
+bot.run(TOKEN)
