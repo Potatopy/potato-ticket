1a7d80fb1f2174d314ae23eea79f14b9caca7a75
Potatopy <baricaz001@outlook.com>
2022-10-23 21:21:56 -0400

    updated .gitignore

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import nextcord
 import asyncio
 from config.json import TOKEN
 
-bot = commands.Bot(command_prefix='-', intents = nextcord.Intents.all())
+bot = commands.Bot(command_prefix='-', intents=nextcord.Intents.all())
 
 @bot.event
 async def on_ready():
-    print('bot is ready!')
+    print('Bot is online!')
 
 bot.run(TOKEN)
